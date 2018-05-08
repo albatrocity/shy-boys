7a9656c642175ff955cadb181ba365f1739c08d0
Ross Brown <ross@cremalab.com>
2018-05-07 20:41:04 -0500

    Fixing cacheBuster

--- models/cacheBuster.js
+++ models/cacheBuster.js
@@ -2,12 +2,12 @@ const keystone = require('keystone')
 
 module.exports = (List) => {
   List.schema.pre('save', function(next) {
-    keystone.list('Configuration').model.update({active: true}, {
+    keystone.list('Configuration').model.collection.updateOne({active: true}, {
       $set: { cache_key: Date.now()}
     }).then(next)
   })
   List.schema.pre('remove', function(next) {
-    keystone.list('Configuration').model.update({active: true}, {
+    keystone.list('Configuration').model.collection.updateOne({active: true}, {
       $set: { cache_key: Date.now() }
     }).then(next)
   })
