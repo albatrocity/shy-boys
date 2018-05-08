78470cbf096297c83aa8911a60e221773420edd9
Ross Brown <ross@cremalab.com>
2018-05-07 20:54:19 -0500

    logs

--- models/cacheBuster.js
+++ models/cacheBuster.js
@@ -2,6 +2,8 @@ const keystone = require('keystone')
 
 module.exports = (List) => {
   List.schema.pre('save', function(next) {
+    console.log('CACHE BUSTER SAVE HOOK');
+    console.log(keystone.list('Configuration').model.collection);
     keystone.list('Configuration').model.collection.update({active: true}, {
       $set: { cache_key: Date.now().toString()}
     }).then(next)
