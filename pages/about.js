8ee88a2aecc9c70a534ca6d93616879361309c0b
Ross Brown <ross@cremalab.com>
2018-06-05 11:55:28 -0500

    Fix images on about page

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,11 @@
-import Head from 'next/head';
-import Layout from '../components/Layout';
-import { setupPushables, handleMouseMove } from '../lib/pushables';
-import fetch from 'isomorphic-unfetch';
-import PostsView from '../components/PostsView';
-import Pagination from '../components/Pagination';
-import { apiUrl } from '../config/urls';
-
+import Head from "next/head";
+import Layout from "../components/Layout";
+import { setupPushables, handleMouseMove } from "../lib/pushables";
+import ImageSlider from "../components/ImageSlider";
+import fetch from "isomorphic-unfetch";
+import PostsView from "../components/PostsView";
+import Pagination from "../components/Pagination";
+import { apiUrl } from "../config/urls";
 
 const About = ({ page }) => (
   <Layout>
@@ -13,21 +13,25 @@ const About = ({ page }) => (
       <meta property="og:title" content={`${page.title} - Shy Boys`} />
       <meta property="og:site_name" content="Shy Boys" />
       <title>{page.title} - Shy Boys</title>
-      <meta property="og:image:url" content={ (page.images[0] || {}).url } key="og:image:url"/>
+      <meta
+        property="og:image:url"
+        content={(page.images[0] || {}).url}
+        key="og:image:url"
+      />
     </Head>
-    { page.images.length > 0 &&
-      <section className='images'>
+    {page.images.length > 0 && (
+      <section className="images">
         <ImageSlider images={page.images} />
       </section>
-    }
+    )}
     <section dangerouslySetInnerHTML={{ __html: page.content }} />
   </Layout>
-)
+);
 
-About.getInitialProps = async (context) => {
-  const res = await fetch(`${apiUrl}/pages/about`)
-  const json = await res.json()
-  return { page: json }
-}
+About.getInitialProps = async context => {
+  const res = await fetch(`${apiUrl}/pages/about`);
+  const json = await res.json();
+  return { page: json };
+};
 
-export default About
+export default About;
