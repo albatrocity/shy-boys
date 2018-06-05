dc59e9f947f6d6afcc0d869c4090760eab149ca5
Ross Brown <ross@cremalab.com>
2018-06-05 10:24:27 -0500

    Adaptive height slick sliders

--- components/ImageSlider.js
+++ components/ImageSlider.js
@@ -2,12 +2,13 @@ import Slick from "react-slick";
 
 const options = {
   infinite: false,
-}
+  adaptiveHeight: true
+};
 
-const ImageSlider = ({images}) => (
-  <Slick {...options} >
-    { images.map(x => <img key={x.public_id} src={x.url} />) }
+const ImageSlider = ({ images }) => (
+  <Slick {...options}>
+    {images.map(x => <img key={x.public_id} src={x.url} />)}
   </Slick>
-)
+);
 
-export default ImageSlider
+export default ImageSlider;
